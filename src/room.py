b10b827cc3525cd517b34a8b0f1b952c5cb3af44
Sherexmykes <sherexmykes@gmail.com>
2020-06-11 22:53:29 -0400

    trying to fix mmvp

--- src/room.py
+++ src/room.py
@@ -1,6 +1,5 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-
 class Room():
     def __init__(self, room_name, description):
         self.room_name = room_name
@@ -9,6 +8,17 @@ class Room():
         self.s_to = None
         self.e_to = None
         self.w_to = None
-
+        self.items = []
+    
     def __str__(self):
         return f' {self.room_name}, {self.description}'
+
+
+    def list_items(self):
+        if not self.items:
+            print("This room has no items.")
+        else:
+            print("This room has: ")
+            for item in self.items:
+                print(item.name)
+                print(item.description)
